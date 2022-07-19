c26e7451a461878708ab0a3653ec6f15fd3bf185
Giorgi Cheishvili <giorgi.cheishvili@outlook.com>
2022-07-19 12:59:57 -0700

    feat(ci): imported features from forked repo (#1)
    * feat(ci): imported features from forked repo
    
    * refactor(ci): renamed on-pull workflow
    
    * refactor(ci): added name to the one_run step

---
+++ .github/actions/npm-publish/index.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core');
+const npmPublish = require('@jsdevtools/npm-publish');
+const { resolve } = require('path');
+
+const packagePaths = core.getInput('packagePaths').split(',').map(path => resolve(__dirname, `../../../${path}/package.json`));
+const isPrerelease = core.getInput('isPrerelease');
+const npmToken = core.getInput('token');
+
+const run = async() => {
+    for (const packagePath of packagePaths) {
+        const result = await npmPublish({
+            package: packagePath,
+            token: npmToken,
+            tag: isPrerelease ? 'prerelease' : 'latest'
+        });
+        core.info(`Published ${result.package}@${result.version}`);
+    }
+};
+
+run();
