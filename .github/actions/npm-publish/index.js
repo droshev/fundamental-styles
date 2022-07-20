792c1a1f72917bed700294a0bda0939a4cdad1e8
g-cheishvili <giorgi.cheishvili@outlook.com>
2022-07-20 15:39:02 +0400

    feat(ci): imported features from main for latest hotfix #2

--- .github/actions/npm-publish/index.js
+++ .github/actions/npm-publish/index.js
@@ -1,17 +1,26 @@
 const core = require('@actions/core');
 const npmPublish = require('@jsdevtools/npm-publish');
-const { resolve } = require('path');
 
-const packagePaths = core.getInput('packagePaths').split(',').map(path => resolve(__dirname, `../../../${path}/package.json`));
-const isPrerelease = core.getInput('isPrerelease');
+const packagePaths = core.getInput('packagePaths').split(',').map(path => `./${path}/package.json`);
+const isPrerelease = core.getInput('isPrerelease') !== 'false';
+const isHotfix = core.getInput('isHotfix') !== 'false';
 const npmToken = core.getInput('token');
 
-const run = async() => {
+let tag = 'latest';
+
+if (isPrerelease) {
+    tag = 'prerelease';
+}
+if (isHotfix) {
+    tag = 'archive';
+}
+
+const run = async () => {
     for (const packagePath of packagePaths) {
         const result = await npmPublish({
             package: packagePath,
             token: npmToken,
-            tag: isPrerelease ? 'prerelease' : 'latest',
+            tag,
             dryRun: true
         });
         core.info(`Published ${result.package}@${result.version}`);
